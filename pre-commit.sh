141bba2c3fcad8230fade89ccca14c97f395c6d8
Chloe Azencott <chloe-agathe.azencott@mines-paristech.fr>
2021-05-19 17:40:33 +0200

    initializing contents

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,15 @@
 rm poly/*.aux 
-rm poly/*.bbl
-rm poly/*.blg
 rm poly/*.log
 rm poly/*.out
+rm poly/*/*.log
+
+rm pc/pc*/*.aux 
+rm pc/pc*/*.log
+rm pc/pc*/*.out
+
 rm poly/*~
 rm poly/*/*~
-rm poly/*/*.log
+rm pc/*/*~
 
 for fname in poly/notebooks/*.ipynb ; do nbstripout $fname ; done
 for fname in pc/pc*/*.ipynb ; do nbstripout $fname ; done
