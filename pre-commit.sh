5b2cd117a20a2c409be90aef13744fed5065df12
Chloe Azencott <chloe-agathe.azencott@mines-paristech.fr>
2021-06-11 14:39:54 +0200

    diapos pour amphi sur chapitre 6

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,6 +4,7 @@ rm -f poly/*.aux
 rm -f poly/*.log
 rm -f poly/*.out
 rm -f poly/*.ent
+rm -f poly/*.nav
 rm -f poly/*.toc
 rm -f poly/*/*.log
 
@@ -11,9 +12,19 @@ rm -f pc/pc*/*.aux
 rm -f pc/pc*/*.log
 rm -f pc/pc*/*.out
 
+rm -f slides/*.aux 
+rm -f slides/*.log
+rm -f slides/*.out
+rm -f slides/*.ent
+rm -f slides/*.nav
+rm -f slides/*.toc
+rm -f slides/*.snm
+
 rm -f poly/*~
 rm -f poly/*/*~
 rm -f pc/*/*~
+rm -f slides/*~
 
 for fname in poly/notebooks/*.ipynb ; do nbstripout $fname ; done
 for fname in pc/pc*/*.ipynb ; do nbstripout $fname ; done
+for fname in slides/*.ipynb ; do nbstripout $fname ; done
